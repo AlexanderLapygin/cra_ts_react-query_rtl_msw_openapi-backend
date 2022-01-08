d8ff589896b7b0366d4965c454ada6271d739940
softspider <sftspider@gmail.com>
2022-01-08 16:09:10 +0300

    refactor: renaming api spec chore: update yarn.lock feat: add OpenAPIBackend code to test

--- src/User/Users.spec.tsx
+++ src/User/Users.spec.tsx
@@ -3,7 +3,27 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { rest } from 'msw';
 import { Users } from './Users';
 import { mockUsers } from '../api/mocks/db';
-import { server } from '../api/mocks/server';
+import {OpenAPIBackend} from "openapi-backend";
+import path from "path";
+import {setupServer} from "msw/node";
+
+// define api
+const api = new OpenAPIBackend({
+  definition: path.join(__dirname, '../../api', 'todo-api.yaml'),
+  handlers: {
+    validationFail: async (c, req, res) => res.status(400).json({ err: c.validation.errors }),
+    notFound: async (c, req, res) => res.status(404).json({ err: 'not found' }),
+    notImplemented: async (c, req, res) => {
+      const { status, mock } = c.api.mockResponseForOperation(c.operation.operationId as string);
+      return res.status(status).json(mock);
+    },
+  },
+});
+
+// tell msw to intercept all requests to api/* with our mock
+const server = setupServer(
+  rest.get('/api/*', (req: any, res, ctx) => api.handleRequest(req, res, ctx))
+);
 
 describe('Users', () => {
   test('renders loading', async () => {
